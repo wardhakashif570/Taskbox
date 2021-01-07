c8937a94fbb67d16e237fed30b3d4027fffdb633
Wardha Kashif <wardhakashif570@gmail.com>
2021-01-07 13:12:46 -0800

    TaskBox

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { Provider } from 'react-redux';
+import store from './lib/redux';
+
+import InboxScreen from './components/InboxScreen';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
+  );
+}
+
+export default App;
